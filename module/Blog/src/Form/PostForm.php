b2d74add702e22c495fb8786b871063ad4335a88
christmas <iury.christmas@gmail.com>
2018-05-02 21:28:20 -0300

    create insert on bd

---
+++ module/Blog/src/Form/PostForm.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: christmas
+ * Date: 02/05/18
+ * Time: 20:38
+ */
+
+namespace Blog\Form;
+
+
+use Zend\Form\Form;
+
+class PostForm extends Form
+{
+    public function __construct($name = null)
+    {
+        parent::__construct('post');
+
+        $this->add([
+            'name'  => 'id',
+            'type'  => 'hidden',
+        ]);
+
+        $this->add([
+            'name'  => 'title',
+            'type'  => 'text',
+            'options' => [
+                'label' => 'Title',
+            ]
+        ]);
+
+        $this->add([
+            'name'  => 'content',
+            'type'  => 'text',
+            'options' => [
+                'label' => 'Content',
+            ]
+        ]);
+
+        $this->add([
+            'name'  => 'submit',
+            'type'  => 'submit',
+            'attributes' => [
+                'value' => 'Go',
+                'id'    => 'submitbutton',
+            ]
+        ]);
+    }
+}
